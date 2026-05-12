d713285d0ab95366f84b5a5f81f3fe3c47efe1f2
b-sch <bartlomiej.schneider@gmail.com>
2026-05-12 20:44:58 +0200

    added webdriver manager support for chrome

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 import logging
 
+from webdriver_manager.chrome import ChromeDriverManager
+
 log = logging.getLogger(__name__)
+
+service = webdriver.ChromeService(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
 
 def test_example(uvicorn_runner):
     log.info("Opening browser")
-    driver = webdriver.Firefox()
     driver.get("http://localhost:8001")
     log.info("Testing if title is correct")
     assert "Pet Library" in driver.title
