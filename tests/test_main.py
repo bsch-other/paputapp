34047e9a97478a18a2bd0e168a574418efa4be57
b-sch <bartlomiej.schneider@gmail.com>
2026-05-14 13:52:48 +0200

    changed to plain Edge driver

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,10 @@
 from selenium import webdriver
 import logging
-
-from webdriver_manager.chrome import ChromeDriverManager
-
 log = logging.getLogger(__name__)
 
-service = webdriver.ChromeService(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
 
 def test_example(uvicorn_runner):
+    driver = webdriver.Edge()
     log.info("Opening browser")
     driver.get("http://localhost:8001")
     log.info("Testing if title is correct")
